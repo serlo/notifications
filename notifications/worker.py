98c16d564b08c7c35bc54184f4e3b8b9eee057de
Anna Bettina Ovsiyovna Steinberg <steinberga@cip.ifi.lmu.de>
2020-02-06 14:47:11 +0100

    worker of Kafka queue implemented

--- notifications/worker.py
+++ notifications/worker.py
@@ -11,10 +11,9 @@ class Message(TypedDict):
 def execute_message(data: Message) -> Message:
     if data["type"] == "create-event":
         create_event(data["payload"])
-    if data["type"] == "create-notification":
+    elif data["type"] == "create-notification":
         create_notification(data["payload"])
-    if data["type"] == "read-notification":
+    elif data["type"] == "read-notification":
         read_notification(data["payload"])
-    return data
-
-    raise Exception("Invalid message")
+    else:
+        raise Exception("Invalid message")
