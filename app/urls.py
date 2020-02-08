fa5470f1884c6083f615b26d4d4044e1bfb16da8
Jonas Keinholz <git@inyono.dev>
2020-02-08 03:40:38 +0100

    only load pact app in debug mode

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("health", views.health, name="health",),
-    # TODO: only load in dev
-    path("pact/", include("pact.urls")),
+    path("health", views.health, name="health"),
     path("", include("notifications.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend([path("pact/", include("pact.urls"))])
